733a9eaa837f386e137e05b49a137b212a0493ec
Errol Ramirez <errol.ramirez@hotmail.co.uk>
2018-01-26 12:00:12 +0000

    Write first unit test, and created empty class to make it compile

---
+++ RomanNumeralTests/RomanNumeralTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RomanNumeralTests
+{
+    public class RomanNumeralTest
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        public void WhenAValueIsPassedItOutputsTheRomanNumeralVersionOfThatNumber(int value, string expectedValue)
+        {
+            RomanNumeralGenerator romanNumeralGenerator = new RomanNumeralGenerator();
+
+            string actualValue = romanNumeralGenerator.GenerateRomanNumeral(value);
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
