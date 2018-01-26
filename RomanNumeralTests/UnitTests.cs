a2bad92712c6dac5cad71a37327638baef896029
Errol Ramirez <eramirez@htec.co.uk>
2018-01-26 11:45:44 +0000

    Initial commit of solution

---
+++ RomanNumeralTests/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace RomanNumeralTests
+{
+
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData(1,"I")]
+        public void WhenIPassInAValueItReturnsARomanNumeral(int value, string expectedOutcome)
+        {
+            //arrange
+            RomanNumeralGenerator romanNumeralGenerator = new RomanNumeralGenerator();
+
+            //act 
+            string actualOutcome = romanNumeralGenerator.GenerateRomanNumeral(value);
+
+            //assert
+            Assert.Equal(expectedOutcome, actualOutcome);
+        }
+    }
+}
